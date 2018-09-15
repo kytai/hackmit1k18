4afc922ce4a45e08a65e3b87df17394414374fbc
Kasaract <35552077+Kasaract@users.noreply.github.com>
2018-09-15 17:28:08 -0400

    Initial Code Layout

---
+++ user.js
@@ -0,0 +1,44 @@
+import time
+import math
+// some comment
+
+var resource = userinput('What resource do you need?');
+var location = userinput(address)
+// for Google Maps API
+
+function avg_transaction_rate(store, resource) {
+	rate = (resource[t] - resource[t-15])/(15)
+	console.log(rate)
+}
+
+function inventory_ct (resource, store) {
+	console.log(number from some database with key-pair [resource, store])
+}
+
+function inven_ct_at_arrival (inventory_ct, avg_transaction_rate) {
+	var arrival = inventory_ct - avg_transaction_rate*travel_times
+}
+
+
+
+var travel times = []
+var localgs = []
+
+localgs.append.googlemaps.find(local groceries stores within 25 mi) 
+
+for i in localgs[0] < i <= localgs[n] {
+	travel times.append.googlemaps.find(travel time)
+} //paired with local grocery store
+
+
+arrival => Decision Optimization API => displayed to user
+
+
+
+
+
+
+
+
+
+
